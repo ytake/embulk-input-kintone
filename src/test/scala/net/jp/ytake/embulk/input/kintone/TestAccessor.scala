4302c4723ffe3b17cd36a448028806a45ac61950
ytake <yuuki.takezawa@comnect.jp.net>
2021-04-27 22:34:20 +0900

    added tests

--- src/test/scala/net/jp/ytake/embulk/input/kintone/TestAccessor.scala
+++ src/test/scala/net/jp/ytake/embulk/input/kintone/TestAccessor.scala
@@ -2,9 +2,10 @@ package net.jp.ytake.embulk.input.kintone
 
 import com.cybozu.kintone.client.model.app.form.FieldType
 import com.cybozu.kintone.client.model.file.FileModel
+import com.cybozu.kintone.client.model.member.Member
 import com.cybozu.kintone.client.model.record.SubTableValueItem
 import com.cybozu.kintone.client.model.record.field.FieldValue
-import com.cybozu.kintone.client.module.file.File
+import scala.jdk.CollectionConverters._
 import org.junit.Assert._
 import org.junit.Test
 
@@ -14,12 +15,61 @@ import java.util
 
 final class TestAccessor {
 
-  private val uniqueKey = 1
+  private val uniqueKey: Int = 1
   private val subtableValue = "{\"row1\":{\"type\":\"SINGLE_LINE_TEXT\",\"value\":\"single line\"},\"multi1\":{\"type\":\"MULTI_SELECT\",\"value\":[\"sample1\",\"sample2\"]}}"
   private val f = new Field
 
   @Test
   def shouldBeReturnedExpectedValues(): Unit = {
+
+    val numberField = new FieldValue
+    numberField.setType(FieldType.NUMBER)
+    numberField.setValue(uniqueKey)
+
+    f
+      .add("文字列__1行", this.makeFieldValue("test single line", FieldType.SINGLE_LINE_TEXT))
+      .add("数値", numberField)
+      .add("文字列__複数行", this.makeFieldValue("test multi text", FieldType.MULTI_LINE_TEXT))
+      .add("リッチテキスト", this.makeFieldValue("<div>test rich text<br /></div>", FieldType.RICH_TEXT))
+      .add("チェックボックス", this.makeCheckbox())
+      .add("table", this.makeTable())
+      .add("file1", this.makeFileList())
+      .add("ラジオボタン", this.makeFieldValue("radio1", FieldType.RADIO_BUTTON))
+      .add("ドロップダウン", this.makeFieldValue("dropdown1", FieldType.DROP_DOWN))
+      .add("複数選択", this.makeMultiSelect())
+      .add("リンク", this.makeFieldValue("https://github.com/", FieldType.LINK))
+      .add("年月日", this.makeFieldValue("2021-01-01", FieldType.DATE))
+      .add("時刻", this.makeFieldValue("12:34", FieldType.TIME))
+      .add("日時", this.makeFieldValue("2021-01-02T03:40Z", FieldType.DATETIME))
+      .add("ユーザー選択", this.makeUserSelect())
+      .add("グループ選択", this.makeGroupSelect())
+      .add("組織選択", this.makeOrganizationSelect())
+      .add("creator", this.makeMember(new Member("code1", "user1"), FieldType.CREATOR))
+      .add("modifier", this.makeMember(new Member("code1", "user1"), FieldType.MODIFIER))
+
+    val accessor = new Accessor(f.getRecords)
+    assertEquals("test single line", accessor.get("文字列__1行"))
+    assertEquals("1", accessor.get("数値"))
+    assertEquals("test multi text", accessor.get("文字列__複数行"))
+    assertEquals("<div>test rich text<br /></div>", accessor.get("リッチテキスト"))
+    assertEquals("sample1\nsample2", accessor.get("チェックボックス"))
+    assertEquals(this.subtableValue, accessor.get("table"))
+    assertEquals("123ecfrwqefknaflwen\n123ecfrwqefknaflwen", accessor.get("file1"))
+    assertEquals("radio1", accessor.get("ラジオボタン"))
+    assertEquals("dropdown1", accessor.get("ドロップダウン"))
+    assertEquals("multi1\nsample2", accessor.get("複数選択"))
+    assertEquals("https://github.com/", accessor.get("リンク"))
+    assertEquals("2021-01-01", accessor.get("年月日"))
+    assertEquals("12:34", accessor.get("時刻"))
+    assertEquals("2021-01-02T03:40Z", accessor.get("日時"))
+    assertEquals("code1\ncode2", accessor.get("ユーザー選択"))
+    assertEquals("code1\ncode2", accessor.get("グループ選択"))
+    assertEquals("code1\ncode2", accessor.get("組織選択"))
+    assertEquals("code1", accessor.get("creator"))
+    assertEquals("code1", accessor.get("modifier"))
+  }
+
+  private def makeTable(): FieldValue = {
     // make table
     val table = new FieldValue
     val tv = new SubTableValueItem
@@ -39,7 +89,10 @@ final class TestAccessor {
     rows.put("multi1", tableRowTwo)
     table.setType(FieldType.SUBTABLE)
     table.setValue(rows)
+    table
+  }
 
+  private def makeFileList(): FieldValue = {
     val fb = new FileModel
     fb.setName("file1")
     fb.setSize("0")
@@ -54,75 +107,71 @@ final class TestAccessor {
     val fv = new FieldValue
     fv.setValue(fl)
     fv.setType(FieldType.FILE)
+    fv
+  }
 
-    val singleLine = new FieldValue
-    singleLine.setType(FieldType.SINGLE_LINE_TEXT)
-    singleLine.setValue("test single line")
-
-    val numberField = new FieldValue
-    numberField.setType(FieldType.NUMBER)
-    numberField.setValue(uniqueKey)
-
-    val multiLine = new FieldValue
-    multiLine.setType(FieldType.MULTI_LINE_TEXT)
-    multiLine.setValue("test multi text")
-
-    val richText = new FieldValue
-    richText.setValue("<div>test rich text<br /></div>")
-    richText.setType(FieldType.RICH_TEXT)
-
+  private def makeCheckbox(): FieldValue = {
     val checkBox = new FieldValue
     val checkList = new util.ArrayList[String]()
     checkList.add("sample1")
     checkList.add("sample2")
     checkBox.setValue(checkList)
     checkBox.setType(FieldType.CHECK_BOX)
-    f
-      .add("文字列__1行", singleLine)
-      .add("数値", numberField)
-      .add("文字列__複数行", multiLine)
-      .add("リッチエディター", richText)
-      .add("チェックボックス", checkBox)
-      .add("table", table)
-      .add("file1", fv)
-      /*
-      .add("ラジオボタン", new RadioButtonFieldValue("radio1"))
-      .add("ドロップダウン", new DropDownFieldValue("dropdown1"))
-      .add("複数選択", new MultiSelectFieldValue(Seq("sample1", "sample2"): _*))
-      .add("リンク", new LinkFieldValue("https://github.com/"))
-      .add("日付", new DateFieldValue(toLocalDate("2021-01-01")))
-      .add("時刻", new TimeFieldValue(toLocalTime("12:34")))
-      .add("日時", new DateTimeFieldValue(toZonedDatetime("2021-01-02T03:40Z")))
-      .add("ユーザー選択", new UserSelectFieldValue(Seq(new User("user1"), new User("user2")): _*))
-      .add("グループ選択", new GroupSelectFieldValue(Seq(new Group("group1"), new Group("group2")): _*))
-      .add("組織選択", new OrganizationSelectFieldValue(Seq(new Organization("org1"), new Organization("org2")): _*))
-      .add("creator", new CreatorFieldValue(new User("user1")))
-      .add("modifier", new ModifierFieldValue(new User("user1")))
-      */
+    checkBox
+  }
 
+  private def makeMultiSelect(): FieldValue = {
+    val multi = new FieldValue
+    val l = new util.ArrayList[String]()
+    l.add("multi1")
+    l.add("sample2")
+    multi.setValue(l)
+    multi.setType(FieldType.MULTI_SELECT)
+    multi
+  }
 
-    val accessor = new Accessor(f.getRecords)
-    assertEquals("test single line", accessor.get("文字列__1行"))
-    assertEquals("1", accessor.get("数値"))
-    assertEquals("test multi text", accessor.get("文字列__複数行"))
-    assertEquals("<div>test rich text<br /></div>", accessor.get("リッチエディター"))
-    assertEquals("sample1\nsample2", accessor.get("チェックボックス"))
-    assertEquals(this.subtableValue, accessor.get("table"))
-    assertEquals("123ecfrwqefknaflwen\n123ecfrwqefknaflwen", accessor.get("file1"))
-    /*
-    assertEquals("radio1", accessor.get("ラジオボタン"))
-    assertEquals("dropdown1", accessor.get("ドロップダウン"))
-    assertEquals("sample1\nsample2", accessor.get("複数選択"))
-    assertEquals("https://github.com/", accessor.get("リンク"))
-    assertEquals("2021-01-01", accessor.get("日付"))
-    assertEquals("12:34", accessor.get("時刻"))
-    assertEquals("2021-01-02T03:40Z", accessor.get("日時"))
-    assertEquals("user1\nuser2", accessor.get("ユーザー選択"))
-    assertEquals("group1\ngroup2", accessor.get("グループ選択"))
-    assertEquals("org1\norg2", accessor.get("組織選択"))
-    assertEquals("user1", accessor.get("creator"))
-    assertEquals("user1", accessor.get("modifier"))
-    */
+  private def makeUserSelect(): FieldValue = {
+    val userSelect = new FieldValue
+    val l = new util.ArrayList[Member]()
+    l.add(new Member("code1", "user1"))
+    l.add(new Member("code2", "user2"))
+    userSelect.setValue(l)
+    userSelect.setType(FieldType.USER_SELECT)
+    userSelect
+  }
+
+  private def makeGroupSelect(): FieldValue = {
+    val userSelect = new FieldValue
+    val l = new util.ArrayList[Member]()
+    l.add(new Member("code1", "group1"))
+    l.add(new Member("code2", "group2"))
+    userSelect.setValue(l)
+    userSelect.setType(FieldType.GROUP_SELECT)
+    userSelect
+  }
+
+  private def makeOrganizationSelect(): FieldValue = {
+    val orgSelect = new FieldValue
+    val l = new util.ArrayList[Member]()
+    l.add(new Member("code1", "org1"))
+    l.add(new Member("code2", "org2"))
+    orgSelect.setValue(l)
+    orgSelect.setType(FieldType.ORGANIZATION_SELECT)
+    orgSelect
+  }
+
+  private def makeMember(m: Member, t: FieldType): FieldValue = {
+    val fv = new FieldValue
+    fv.setValue(m)
+    fv.setType(t)
+    fv
+  }
+
+  private def makeFieldValue(o: Object, t: FieldType): FieldValue = {
+    val field = new FieldValue
+    field.setValue(o)
+    field.setType(t)
+    field
   }
 
   private def toLocalDate(ds: String): LocalDate = LocalDate.parse(ds, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
